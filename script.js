5ccd9b3d99cc67b50388b47aa402b48dd27fafc7
MorvidAngel <5d7n2s9m6b@live.com>
2022-08-02 23:59:07 -0400

    working on js validator

---
+++ script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+}
